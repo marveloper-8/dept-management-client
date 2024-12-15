1a1ed13473e2219aa1fd6d44dc1028828999ec35
Josh <marveloper.8@gmail.com>
2024-12-14 18:31:51 -0800

    setup - graphql and apollo setup

---
+++ src/graphql/queries.ts
@@ -0,0 +1,14 @@
+import { gql } from "@apollo/client";
+
+export const GET_DEPARTMENTS = gql`
+    query GetDepartments {
+        departments {
+            id
+            name
+            subDepartments {
+                id
+                name
+            }
+        }
+    }
+`;
